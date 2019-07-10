35fb1cdaca7aae576a66cb9733681e937a6cbedf
comsince <liaojinlong@meizu.com>
2019-07-10 17:17:23 +0800

    修改七牛云cnd域名配置

--- spring-boot-dubbo-push-connector/src/main/java/com/comsince/github/configuration/MediaServerConfig.java
+++ spring-boot-dubbo-push-connector/src/main/java/com/comsince/github/configuration/MediaServerConfig.java
@@ -6,19 +6,19 @@ public class MediaServerConfig {
 	public static String QINIU_SECRET_KEY = "3hMHnFAxu6vIbjqbvEI4K-zw7BK614mYZFqKhU-B";
 
     public static String QINIU_BUCKET_GENERAL_NAME = "qiniu_bucket_general_name";
-    public static String QINIU_BUCKET_GENERAL_DOMAIN = "http://ptszben6b.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_GENERAL_DOMAIN = "http://general.comsince.cn/";
     public static String QINIU_BUCKET_IMAGE_NAME = "qiniu_bucket_image_name";
-    public static String QINIU_BUCKET_IMAGE_DOMAIN = "http://ptsyum6g1.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_IMAGE_DOMAIN = "http://image.comsince.cn/";
     public static String QINIU_BUCKET_VOICE_NAME = "qiniu_bucket_voice_name";
-    public static String QINIU_BUCKET_VOICE_DOMAIN = "http://ptsrknsxc.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_VOICE_DOMAIN = "http://voice.comsince.cn/";
     public static String QINIU_BUCKET_VIDEO_NAME = "qiniu_bucket_video_name";
-    public static String QINIU_BUCKET_VIDEO_DOMAIN =  "http://ptsyunkd0.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_VIDEO_DOMAIN =  "http://video.comsince.cn/";
     public static String QINIU_BUCKET_FILE_NAME = "qiniu_bucket_file_name";
-    public static String QINIU_BUCKET_FILE_DOMAIN = "http://ptsyrs75s.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_FILE_DOMAIN = "http://file.comsince.cn/";
     public static String QINIU_BUCKET_PORTRAIT_NAME = "qiniu_bucket_portrait_name";
-    public static String QINIU_BUCKET_PORTRAIT_DOMAIN = "http://ptsq6icgi.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_PORTRAIT_DOMAIN = "http://portrait.comsince.cn/";
     public static String QINIU_BUCKET_FAVORITE_NAME = " qiniu_bucket_favorite_name";
-    public static String QINIU_BUCKET_FAVORITE_DOMAIN = "http://ptszqfzms.bkt.clouddn.com/";
+    public static String QINIU_BUCKET_FAVORITE_DOMAIN = "http://favorite.comsince.cn/";
     
     public static String SERVER_IP = "localhost";
     public static int HTTP_SERVER_PORT = 8080;
